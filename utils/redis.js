814a95173b47c7278322eb5b7e6cae46f54dd097
Carey99 <edwinakidah1@gmail.com>
2024-09-03 14:05:29 +0300

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,62 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+      console.error('Redis client Error:', err)
+    });
+
+    this.client.connect().catch((err) => {
+      console.error('Failed to connect', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.isOpen;
+  }
+
+  async get(key) {
+    if (!this.isAlive()) {
+        await this.client.connect();
+      }
+    try {
+      const val = await this.client.get(key);
+      return val;
+    } catch(err) {
+      console.error(err);
+      return null;
+    }
+  }
+
+  async set(key, value, duration) {
+    if (!this.isAlive()) {
+      await this.client.connect();
+    }
+    try {
+      if (duration) {
+        await this.client.set(key, value, { EX: duration });
+      } else {
+        await this.client.set(key, value);
+      }
+    } catch(err) {
+      console.log(err);
+      return null;
+    }
+  }
+
+  async del(key) {
+    if (!this.isAlive()) {
+      await this.client.connect();
+    }
+
+    try {
+      await this.client.del(key);
+    } catch(err) {
+      console.error(err)
+    }
+  }
+}
+
+module.exports = new RedisClient();
