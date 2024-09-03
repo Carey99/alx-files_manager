adeeb7d8fc79cbc41fc590d38a72f76e652559b2
Carey99 <edwinakidah1@gmail.com>
2024-09-03 17:58:37 +0300

    First API

---
+++ controllers/AppController.js
@@ -0,0 +1,26 @@
+import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js';
+
+class AppController {
+  static async getStatus(req, res) {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
+
+    res.ststus(200).json({ redis: redisStatus, db: dbStatus })
+  }
+
+  static async getStats(req, res) {
+    try {
+      const users = await dbClient.nbUsers();
+
+      const files = await dbClient.nbFiles();
+
+      res.status(200).json({ users, files });
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+export default AppController;
