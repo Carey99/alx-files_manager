fd60369cb983e8e9fba8a1f8630160e55511670e
Carey99 <edwinakidah1@gmail.com>
2024-09-04 20:33:44 +0300

    New User

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,61 +1,29 @@
-import { ObjectId } from 'mongodb';
 import sha1 from 'sha1';
-import Queue from 'bull';
 import dbClient from '../utils/db';
-import userUtils from '../utils/user';
-
-const userQueue = new Queue('userQueue');
 
 class UsersController {
-  static async postNew(request, response) {
-    const { email, password } = request.body;
+  static async postNew(req, res) {
+    const { email, password} = req.body;
 
-    if (!email) return response.status(400).send({ error: 'Missing email' });
-
-    if (!password) { return response.status(400).send({ error: 'Missing password' }); }
-
-    const emailExists = await dbClient.usersCollection.findOne({ email });
-
-    if (emailExists) { return response.status(400).send({ error: 'Already exist' }); }
-
-    const sha1Password = sha1(password);
-
-    let result;
-    try {
-      result = await dbClient.usersCollection.insertOne({
-        email,
-        password: sha1Password,
-      });
-    } catch (err) {
-      await userQueue.add({});
-      return response.status(500).send({ error: 'Error creating user.' });
+    if (!email) {
+      return res.status(400).send({ error: 'Misssing email' });
     }
+    if (!password) {
+      return res.status(400).send({ error: 'Missing password' });
+    }
+    try {
+      const existingUser = await dbClient.userColle.findOne({ email });
+      if (existingUser) {
+        return res.status(400).send({ error: 'Already exist' });
+      }
 
-    const user = {
-      id: result.insertedId,
-      email,
-    };
-
-    await userQueue.add({
-      userId: result.insertedId.toString(),
-    });
-
-    return response.status(201).send(user);
-  }
-  static async getMe(request, response) {
-    const { userId } = await userUtils.getUserIdAndKey(request);
-
-    const user = await userUtils.getUser({
-      _id: ObjectId(userId),
-    });
-
-    if (!user) return response.status(401).send({ error: 'Unauthorized' });
-
-    const processedUser = { id: user._id, ...user };
-    delete processedUser._id;
-    delete processedUser.password;
-
-    return response.status(200).send(processedUser);
+      const  hashedpasswd = sha1(password);
+      const newUser = await dbClient.userColle.insertOne({ email, password: hashedpasswd });
+      return res.status(201).json({ id: newUser.insertedId, email });
+    } catch(err) {
+      console.error('Error creating user:', err.message);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 }
 
